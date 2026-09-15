e2ab08e198fa141055b1082dfc732e87a8e1afd8
owlillp <d.georgeo.on@gmail.com>
2026-09-15 22:39:57 +0300

    Add OpenApiExtensions

---
+++ src/Shared.Framework/OpenApi/OpenApiExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+
+namespace Shared.Framework.OpenApi;
+
+public static class OpenApiExtensions
+{
+    public static IServiceCollection AddOpenApiSpec(this IServiceCollection services, string title, string version)
+    {
+        services.AddOpenApi(version, options =>
+        {
+            options.AddDocumentTransformer((document, _, _) =>
+            {
+                document.Info = new OpenApiInfo
+                {
+                    Title = title,
+                    Version = version,
+                };
+                return Task.CompletedTask;
+            });
+        });
+
+        return services;
+    }
+}
